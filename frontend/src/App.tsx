316424bf76de13333777c86aa87a4ec65489e3a4
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2026-08-18 18:35:32 +0100

    Menu component created

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { ThemeContextProvider } from './contexts/ThemeContextProvider';
 import { BrowserRouter } from 'react-router-dom';
 import { Header } from './components/Header';
+import { Menu } from './components/Menu';
 import { Router } from './routes/Router';
 import { GlobalStyle } from './styles/Global';
 
@@ -10,6 +11,7 @@ export function App() {
       <ThemeContextProvider>
         <BrowserRouter>
           <Header />
+          <Menu />
           <Router />
         </BrowserRouter>
 
