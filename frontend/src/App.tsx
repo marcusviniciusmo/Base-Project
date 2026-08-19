b593ad38a873404196363e5520534a1ff76f6ed3
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2026-08-19 06:37:25 +0100

    Footer component created

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { Header } from './components/Header';
 import { Menu } from './components/Menu';
 import { Router } from './routes/Router';
+import { Footer } from './components/Footer';
 import { GlobalStyle } from './styles/Global';
 
 export function App() {
@@ -13,6 +14,7 @@ export function App() {
           <Header />
           <Menu />
           <Router />
+          <Footer />
         </BrowserRouter>
 
         <GlobalStyle />
