07ff3454309677597f56ad115a2dc58bf5627d1d
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2026-08-19 06:49:33 +0100

    Routes layout definition

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,6 @@
 import { ThemeContextProvider } from './contexts/ThemeContextProvider';
 import { BrowserRouter } from 'react-router-dom';
-import { Header } from './components/Header';
-import { Menu } from './components/Menu';
 import { Router } from './routes/Router';
-import { Footer } from './components/Footer';
 import { GlobalStyle } from './styles/Global';
 
 export function App() {
@@ -11,10 +8,7 @@ export function App() {
     <>
       <ThemeContextProvider>
         <BrowserRouter>
-          <Header />
-          <Menu />
           <Router />
-          <Footer />
         </BrowserRouter>
 
         <GlobalStyle />
--- frontend/src/routes/Router/index.tsx
+++ frontend/src/routes/Router/index.tsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
+import { DefaultLayout } from '../../layouts/DefaultLayout';
 import { Home } from '../../pages/Home';
 import { NotFound } from '../../pages/NotFound';
 import { routes } from '../Routes';
@@ -6,8 +7,10 @@ import { routes } from '../Routes';
 export function Router() {
   return (
     <Routes>
-      <Route path={routes.home} element={<Home />} />
-      <Route path={routes.notFound} element={<NotFound />} />
+      <Route path='/' element={<DefaultLayout />}>
+        <Route path={routes.home} element={<Home />} />
+        <Route path={routes.notFound} element={<NotFound />} />
+      </Route>
     </Routes>
   );
 }
